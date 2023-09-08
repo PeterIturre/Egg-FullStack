302f409430c263f743dee0584b18c5289461dba4
Pedro Iturre <pedronicolasiturre@gmail.com>
2023-09-08 16:41:56 -0300

    React 2.0

--- Ejericicios/Frontend/React/egg-app/src/App.jsx
+++ Ejericicios/Frontend/React/egg-app/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  let contador2 = 0;
 
+  function sumarCount() {
+    setCount(count + 1);
+  }
+
+  function sumarContador2() {
+    console.log(contador2);
+    contador2++;
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <p>{count}</p>
+      <button onClick={sumarCount}>Sumar count</button>
+      <p>{contador2}</p>
+      <button onClick={sumarContador2}>Sumar contador2</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
